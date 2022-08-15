1ec8fb6a926cd7a651cbf234ba556a607180682f
23122000 <109691496+23122000@users.noreply.github.com>
2022-08-15 23:32:53 +0530

    Add files via upload

---
+++ assignment 1.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+
+{
+printf("hello \n students \n");
+printf("\" MySirG \"");
+printf("\n");
+printf("%%d");
+printf("\n");
+printf("\\\\");
+printf("\n");
+printf("\\n");
+printf("\n");
+
+float a, r;
+printf(" enter the radius\n");
+scanf("%f",&r);
+a =3.14*r*r;
+printf("Area of circle is %f having the radius %f",a,r);
+printf("\n");
+
+
+
+int x = printf("goodluck \n");
+printf("%d",x);
+printf("\n");
+
+
+int hour , minute;
+printf("enter time ");
+scanf("%d:%d",&hour,&minute);
+printf("%d hour and %d minute ",hour,minute);
+
+printf("\n");
+
+int day,month,year;
+ printf("enter todays  date \n");
+ scanf("%d/%d/%d",&day,&month,&year);
+ printf("day - %d, month - %d, year -%d",day,month,year);
+
+
+printf("\n");
+char name[25];
+ printf("enter name \n");
+ scanf("%s",&name);
+ printf("\"hello,%s\"", name);
+
+
+
+
+
+    return 0;
+}
+
